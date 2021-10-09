cba19b021e025b939a54d25d8c03e761537ca4f6
rshri <rshriharripriya19@gmail.com>
2021-10-09 15:44:21 +0530

    added social media links

--- nutpam 2021/template/themes/js/index.js
+++ nutpam 2021/template/themes/js/index.js
@@ -104,12 +104,12 @@ $(function(){
     $('#about-link').on('click',function(){
       gsap.to('#navigation-content',0,{display:"none",delay:.7});
       gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-  gsap.to('#header',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
+  gsap.to('#header',0,{display:"block",delay:.7});
+gsap.to('#blog',0,{display:"block",delay:.7});
+gsap.to('#portfolio',0,{display:"block",delay:.7});
    gsap.to('#breaker',0,{display:"block"});
    gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#contact',0,{display:"none"});
+gsap.to('#contact',0,{display:"block",delay:.7});
    gsap.to('#breaker',0,{display:"none",delay:2});
    gsap.to('#breaker-two',0,{display:"none",delay:2});
    gsap.to('#about',0,{display:"block",delay:.7});
@@ -118,10 +118,10 @@ gsap.to('#contact',0,{display:"none"});
  $('#contact-link').on('click',function(){
    gsap.to('#navigation-content',0,{display:"none",delay:.7});
    gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
+gsap.to('#header',0,{display:"block",delay:.7});
+gsap.to('#about',0,{display:"block",delay:.7});
+gsap.to('#blog',0,{display:"block",delay:.7});
+gsap.to('#portfolio',0,{display:"block",delay:.7});
 gsap.to('#breaker',0,{display:"block"});
 gsap.to('#breaker-two',0,{display:"block",delay:.1});
 gsap.to('#breaker',0,{display:"none",delay:2});
@@ -132,10 +132,10 @@ gsap.to('#navigation-content',0,{display:'flex',delay:2});
 $('#portfolio-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
+gsap.to('#header',0,{display:"block",delay:.7});
+gsap.to('#about',0,{display:"block",delay:.7});
+gsap.to('#contact',0,{display:"block",delay:.7});
+gsap.to('#blog',0,{display:"block",delay:.7});
 gsap.to('#breaker',0,{display:"block"});
 gsap.to('#breaker-two',0,{display:"block",delay:.1});
 gsap.to('#breaker',0,{display:"none",delay:2});
@@ -144,12 +144,12 @@ gsap.to('#portfolio',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
 })
 $('#blog-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
+    gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
+gsap.to('#header',0,{display:"block",delay:.7});
+gsap.to('#about',0,{display:"block",delay:.7});
+gsap.to('#portfolio',0,{display:"block",delay:.7});
+gsap.to('#contact',0,{display:"block",delay:.7});
 gsap.to('#breaker',0,{display:"block"});
 gsap.to('#breaker-two',0,{display:"block",delay:.1});
 gsap.to('#breaker',0,{display:"none",delay:2});
