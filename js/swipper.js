aa95f27947fac70e770e79588922da7a0eb04006
abuthahir-19 <abuthahirstar1@gmail.com>
2021-10-16 08:49:25 +0530

    added event coordinator popup

---
+++ js/swipper.js
@@ -0,0 +1,17 @@
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+    invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+    }
+});
+
+exports.default = swiper;
