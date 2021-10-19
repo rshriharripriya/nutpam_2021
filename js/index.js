864ed8b957f8f5fc215350c42f5b8364d1124c11
rshri <rshriharripriya19@gmail.com>
2021-10-19 13:11:23 +0530

    updated the team scrolling

--- js/index.js
+++ js/index.js
@@ -343,3 +343,143 @@ var setNumber = function (digit, number, on) {
 	}
 };
 
+
+
+window.addEventListener("load", event => {
+
+    var swiperWrapper = document.querySelector('.swiper-wrapper');
+
+    /* The Team */
+    var team = [{
+            name: "Alice Stone",
+            role: "UI Designer",
+            photo: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+        {
+            name: "Adam Turner",
+            role: "Project Manager",
+            photo: "https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+        {
+            name: "Nancy Hughes",
+            role: "UX Specialist",
+            photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+        {
+            name: "Jonathan Campbell",
+            role: "Front-End Developer",
+            photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+        {
+            name: "Jack Keller",
+            role: "Back-End Developer",
+            photo: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+        {
+            name: "Sara Carroll",
+            role: "Head of UI Design",
+            photo: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+         {
+            name: "Sara Carroll",
+            role: "Head of UI Design",
+            photo: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300",
+
+        },
+    ];
+
+
+
+    /* Function to populate the team members */
+    function addTeam() {
+        for (let i = 0; i < 6; i++) {
+
+            /* Variables for the team */
+            var name = team[i].name,
+                role = team[i].role,
+                desc = team[i].desc,
+                photo = team[i].photo
+
+
+            /* Template for the Team Cards */
+            var template = `
+                <div class="swiper-slide">
+                <div class="card">
+                    <span class="bg"></span>
+                    <figure class="photo"><img src="${photo}"></figure>
+                        <article class="text">
+                            <p class="name">${name}</p>
+                            <p class="role">${role}</p> 
+                          
+                        </article>
+                      </div>
+                </div>`;
+
+            swiperWrapper.insertAdjacentHTML('beforeend', template);
+        }
+    };
+
+
+    addTeam();
+
+
+
+    /* Swiper Settings */
+
+    var mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+        centeredSlides: true,
+        speed: 800,
+        slidesPerView: 4,
+        spaceBetween: 40,
+        threshold: 100,
+
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            1180: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+                centeredSlides: true,
+                loop: true,
+            },
+            799: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+                centeredSlides: true,
+                loop: true,
+            },
+        }
+    });
+
+
+
+   function followCursor(event) {
+        var pointer = event.currentTarget.closest(".swiper-slide").querySelector('.pointer'),
+            index = event.currentTarget.dataset.index,
+            sizeIcon = (60 * index) + 25;
+
+        pointer.style.transform = `translateX(${sizeIcon}px)`;
+    }
+
+
+    /* end */
+});
