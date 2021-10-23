5c559bdff7f1aa07aa66a02c8ba17e2870da0e8c
rshri <rshriharripriya19@gmail.com>
2021-10-23 22:17:42 +0530

    removed white html bg

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#portfolio',0,{display:"block",delay:.7});
 gsap.to('#contact',0,{display:"block",delay:.7});
 gsap.to('#blog',0,{display:"block",delay:.7})
- // gsap.to('#navigation-content',0,{display:"none"});
+// gsap.to('#navigation-content',0,{display:"none"});
 //   gsap.to('#navigation-content',0,{display:"flex",delay:1});
 //     gsap.to('#navigation-content',0,{display:"none",delay:.7});
 //   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
