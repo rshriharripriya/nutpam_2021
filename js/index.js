79bc8736141c56c071c21277df27ce216ce5527b
rshri <rshriharripriya19@gmail.com>
2021-11-01 23:03:30 +0530

    ADDED NEW LOGO

--- js/index.js
+++ js/index.js
@@ -341,17 +341,8 @@ var swiper = new Swiper(".mySwiper", {
 
 
 var mybutton = document.getElementById("myBtn");
-
-
-
-
 // When the user scrolls down 20px from the top of the document, show the button
-
 window.onscroll = function() {scrollFunction()};
-
-
-
-
 function scrollFunction() {
 
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
